9bb0965b0688fccdcb0cccda19729bf4631e20bb
ch6vip <ch6vip@gmail.com>
2025-10-14 04:16:18 +0800

    test

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./web/templates/**/*.html",
+    "./web/static/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
